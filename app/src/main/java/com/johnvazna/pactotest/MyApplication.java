d19f8a414afa24efeba844f7cc85a89e1140060d
John <johnvazna@gmail.com>
2024-03-14 14:12:22 -0600

    Added initial project configuration

---
+++ app/src/main/java/com/johnvazna/pactotest/MyApplication.java
@@ -0,0 +1,10 @@
+package com.johnvazna.pactotest;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+    //Hilt initialization with no constructor
+}
