61515f660708d32acdc49cef8c26e4321c03f433
John <johnvazna@gmail.com>
2024-03-15 17:42:41 -0600

    Implementation of search users adapter

--- app/src/main/java/com/johnvazna/pactotest/bases/BaseFragment.java
+++ app/src/main/java/com/johnvazna/pactotest/bases/BaseFragment.java
@@ -11,10 +11,19 @@ public abstract class BaseFragment<T extends BaseViewModel> extends Fragment {
 
     protected abstract T getViewModel();
 
+    protected void showErrorDialog() {
+        BaseDialogFragment dialogFragment = new BaseDialogFragment();
+        dialogFragment.show(getChildFragmentManager(), "errorDialog");
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getViewModel().state.observe(getViewLifecycleOwner(), result -> {
+            if (result.isError()) {
+                showErrorDialog();
+            }
+
             if (result.isLoading()) {
                 ((BaseActivity) requireActivity()).showLoading();
             } else {
