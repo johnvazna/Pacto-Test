710ec874168737ddf4631493eea7638fed8f7a04
John <johnvazna@gmail.com>
2024-03-15 00:28:50 -0600

    Implmentation of GetUsersUseCase

---
+++ data/users/src/main/java/com/johnvazna/data/users/UsersRepository.java
@@ -0,0 +1,41 @@
+package com.johnvazna.data.users;
+
+import com.johnvazna.data.users.mappers.UserMapper;
+import com.johnvazna.data.users.remote.UsersRemoteDataSource;
+import com.johnvazna.data.users.remote.entities.UserDto;
+import com.johnvazna.domain.users.UsersDataSource;
+import com.johnvazna.domain.users.entities.User;
+import com.johnvazna.network.utils.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.rxjava3.core.Single;
+
+public class UsersRepository implements UsersDataSource {
+
+    private final UsersRemoteDataSource usersRemoteDataSource;
+
+    @Inject
+    public UsersRepository(UsersRemoteDataSource usersRemoteDataSource) {
+        this.usersRemoteDataSource = usersRemoteDataSource;
+    }
+
+    @Override
+    public Single<Result<List<User>>> getUsers() {
+        return usersRemoteDataSource.getUsers()
+                .map(result -> {
+                    if (result.isSuccess()) {
+                        List<User> userList = new ArrayList<>();
+                        for (UserDto dto : result.getData()) {
+                            userList.add(UserMapper.fromDto(dto));
+                        }
+                        return Result.success(userList);
+                    } else {
+                        return Result.error(result.getError());
+                    }
+                });
+    }
+}
